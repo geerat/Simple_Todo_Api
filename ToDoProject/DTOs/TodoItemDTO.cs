20d2bd8e3c34642eab1f5c1ad445330f171838da
geerat <chester.jerrat@beca.com>
2020-12-16 09:32:56 +1300

    added DTO for todoItem

--- ToDoProject/DTOs/TodoItemDTO.cs
+++ ToDoProject/DTOs/TodoItemDTO.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ToDoProject.Models
+namespace ToDoProject.DTOs
 {
-    public class TodoItem
+    public class TodoItemDTO
     {
 
         public long Id { get; set; }
-        public string Name { get; set; }
-        public bool IsComplete { get; set; }
 
-        public string Secret { get; set; }
+        public string Name { get; set; }
+
+        public bool IsComplete { get; set; }
 
     }
 }
